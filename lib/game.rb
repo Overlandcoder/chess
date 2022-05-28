db9bacd3abfe7ce326a603d83b02a683f3abb491
Overlander <anuragnandal@hotmail.com>
2022-05-28 08:02:39 -0400

    Add color argument to #create_player

--- lib/game.rb
+++ lib/game.rb
@@ -1,25 +1,56 @@
 require_relative 'player'
+require_relative 'board'
 
 class Game
+  attr_reader :board, :current_player
+
+  def initialize
+    @board = Board.new
+  end
+
   def setup
     intro_message
-    @player1 = create_player(1)
-    @player2 = create_player(2)
+    @player1 = create_player(1, 'white')
+    @player2 = create_player(2, 'black')
+    create_pieces(player1)
+    create_pieces(player2)
   end
 
   def intro_message
-    puts <<~HEREDOC
+    puts <<~INTRO
 Welcome to Chess!
 
 This is a two-player game. The game begins after each player's name has been entered.
 
 Good luck and have fun!
-    HEREDOC
+    INTRO
   end
 
-  def create_player(number)
+  def create_player(number, color)
     puts "\nPlayer #{number}, enter your name:"
     name = gets.chomp.capitalize
-    Player.new(name)
+    Player.new(name, color)
+  end
+
+  def create_pieces(player)
+
+  end
+
+  def play_game
+    setup
+
+    until board.game_over?
+      play_round
+      @current_player = switch_turns
+    end
+    conclusion
+  end
+
+  def play_round
+    board.display
+  end
+
+  def switch_turns
+    current_player == player1 ? player2 : player1
   end
 end
