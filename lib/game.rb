40213d701694ac50968da008432a003d09ddf61e
Overlander <anuragnandal@hotmail.com>
2022-06-03 19:38:02 -0400

    Add methods that enable piece creation

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,12 @@
 require_relative 'player'
 require_relative 'board'
+require_relative 'piece'
+require 'pry-byebug'
 
 class Game
-  attr_reader :board, :current_player
+  attr_reader :board, :current_player, :player1, :player2
+
+  TYPES = ['rook']
 
   def initialize
     @board = Board.new
@@ -12,28 +16,31 @@ class Game
     intro_message
     @player1 = create_player(1, 'white')
     @player2 = create_player(2, 'black')
-    create_pieces(player1)
-    create_pieces(player2)
-  end
-
-  def intro_message
-    puts <<~INTRO
-Welcome to Chess!
-
-This is a two-player game. The game begins after each player's name has been entered.
-
-Good luck and have fun!
-    INTRO
+    pieces(player1.color, player1)
+    pieces(player2.color, player2)
+    board.add_pieces_to_display(player1.pieces)
+    board.add_pieces_to_display(player2.pieces)
+    board.display
   end
 
   def create_player(number, color)
     puts "\nPlayer #{number}, enter your name:"
-    name = gets.chomp.capitalize
+    # name = gets.chomp.capitalize
+    name = 'a'
     Player.new(name, color)
   end
 
-  def create_pieces(player)
+  def pieces(color, player)
+    TYPES.each { |type| create_piece(type, color, player) }
+  end
 
+  def create_piece(type, color, player, i = 1)
+    i = 7 if type == 'pawn'
+
+    (0..i).each do |number|
+      piece = Piece.for(type, color, number)
+      player.add_piece(piece)
+    end
   end
 
   def play_game
@@ -53,4 +60,16 @@ Good luck and have fun!
   def switch_turns
     current_player == player1 ? player2 : player1
   end
+
+  def intro_message
+    puts <<~INTRO
+Welcome to Chess!
+
+This is a two-player game. The game begins after each player's name has been entered.
+
+Good luck and have fun!
+    INTRO
+  end
 end
+
+game = Game.new.setup
