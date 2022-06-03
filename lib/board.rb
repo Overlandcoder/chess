88715b4b0587b6525bbe34187cb9385fc0b71652
Overlander <anuragnandal@hotmail.com>
2022-06-03 19:26:46 -0400

    Add method to display pieces on board

--- lib/board.rb
+++ lib/board.rb
@@ -8,38 +8,48 @@ class Board
   end
 
   def display
+    add_background_color
     content = grid.clone
     content.each_with_index { |row, idx| row.unshift('  ', 8 - idx, ' ') }
     content.each_with_index { |row, idx| row.append(' ', 8 - idx, '  ') }
 
-    puts '    ' + ('A'..'H').to_a.join(' ') + '     '
+    puts '     ' + ('A'..'H').to_a.join('  ') + '     '
     (0..7).each { |i| puts content[i].join }
-    puts '    ' + ('A'..'H').to_a.join(' ') + '     '
+    puts '     ' + ('A'..'H').to_a.join('  ') + '     '
   end
 
-  def background_color
-    grid.each_with_index do |row, idx|
+  def add_background_color
+    grid_clone = grid.clone
+    grid_clone.each_with_index do |row, idx|
       if idx.even?
         row.each_with_index do |val, idx|
           if idx.even?
-            row[idx] = "\u001b[47;1m  \u001b[0m"
+            piece = row[idx]
+            row[idx] = "\u001b[47m #{piece} \u001b[0m"
           else
-            row[idx] = "\u001b[42m  \u001b[0m"
+            piece = row[idx]
+            row[idx] = "\u001b[42m #{piece} \u001b[0m"
           end
         end
       else
         row.each_with_index do |val, idx|
           if idx.odd?
-            row[idx] = "\u001b[47;1m  \u001b[0m"
+            piece = row[idx]
+            row[idx] = "\u001b[47m #{piece} \u001b[0m"
           else
-            row[idx] = "\u001b[42m  \u001b[0m"
+            piece = row[idx]
+            row[idx] = "\u001b[42m #{piece} \u001b[0m"
           end
         end
       end
     end
   end
-end
 
-board = Board.new
-board.background_color
-board.display
+  def add_pieces_to_display(pieces)
+    pieces.each do |piece|
+      row = piece.position[0]
+      column = piece.position[1]
+      grid[row][column] = piece.symbol
+    end
+  end
+end
