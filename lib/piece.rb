0edbc3dc0c630a8f4d9114ac7e0e800205d33303
Overlander <anuragnandal@hotmail.com>
2022-06-03 19:28:36 -0400

    Add factory method for rook

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,14 +1,10 @@
 require_relative 'rook'
 
 class Piece
-  def self.for(type, i = 1)
-    i = 7 if type == 'pawn'
-
-    (0..i).each do |number|
-      case type
-      when 'rook'
-        Rook
-      end.new(number)
-    end
+  def self.for(type, color, number)
+    case type
+    when 'rook'
+      Rook
+    end.new(color, number)
   end
 end
