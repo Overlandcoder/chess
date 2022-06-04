b5f358249198d2f3d2276d16a9c580e8204e99a0
Overlander <anuragnandal@hotmail.com>
2022-06-04 08:58:29 -0400

    Begin adding methods to check move validity

--- lib/rook.rb
+++ lib/rook.rb
@@ -1,5 +1,5 @@
 class Rook
-  attr_reader :color, :number, :position
+  attr_reader :color, :number, :position, :destination
 
   def initialize(color, number)
     @color = color
@@ -10,9 +10,47 @@ class Rook
 
   def symbol
     if color == 'white'
-      "\u265C"
+      "\u001b[37;1m\u265C"
     elsif color == 'black'
       "\u001b[30m\u265C"
     end
+  end
+
+  def change_position(destination)
+    @destination = destination
+
+    if valid_move?
+      
+    end
+  end
+
+  def valid_move?
+    within_board? && path_empty? && (vertical_move? || horizontal_move?)
+  end
+
+  def path_empty?
+    if vertical_move?
+      start_row = position[0]
+      end_row = destination[0]
+      column = position[1]
+
+      (start_row..end_row).all? do |row|
+        grid[row][column] == "\u001b[47m   \u001b[0m" || grid[row][column] == "\u001b[42m   \u001b[0m"
+      end
+    elsif horizontal_move?
+
+    end
+  end
+
+  def within_board?
+    destination[0].between?(0, 7) && destination[1].between?(0, 7)
+  end
+
+  def vertical_move?
+    destination[0] != position[0] && destination[1] == position[1]
+  end
+
+  def horizontal_move?
+    destination[0] == position[0] && destination[1] != position[1]
   end
 end
