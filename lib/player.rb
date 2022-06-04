ad10cbbecf1268a9cf42189e80a27fdb64b26820
Overlander <anuragnandal@hotmail.com>
2022-06-04 08:57:03 -0400

    Add #add_piece and an array to hold pieces

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,13 @@
 class Player
-  attr_reader :name
+  attr_reader :name, :color, :pieces
 
   def initialize(name, color)
     @name = name
     @color = color
+    @pieces = []
+  end
+
+  def add_piece(piece)
+    @pieces << piece
   end
 end
