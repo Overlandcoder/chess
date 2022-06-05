fbeeadfa6a77a088093a9fa9cfdb364351e1547e
Overlander <anuragnandal@hotmail.com>
2022-06-05 11:19:36 -0400

    Refactor display and background coloring

--- lib/board.rb
+++ lib/board.rb
@@ -4,47 +4,77 @@ class Board
   attr_reader :grid
 
   def initialize
-    @grid = Array.new(8) { Array.new(8, ' ') }
+    @grid = Array.new(8) { Array.new(8) }
   end
 
   def display
     add_background_color
-    puts '     ' + ('A'..'H').to_a.join('  ') + '     '
-    (1..8).each { |i| puts '  ' +  (9 - i).to_s + ' ' + grid[i - 1].join + ' ' + (9 - i).to_s + '  ' }
+    column_headings
+
+    (0..7).each do |row|
+      puts "  #{8 - row} #{@grid_clone[row].join} #{8 - row}  "
+    end
+
+    column_headings
+    p grid[0].join
+  end
+
+  def column_headings
     puts '     ' + ('A'..'H').to_a.join('  ') + '     '
   end
 
   def add_background_color
-    grid.each_with_index do |row, idx|
+    @grid_clone = grid.map(&:clone)
+    @grid_clone.each_with_index do |row, idx|
       if idx.even?
         row.each_with_index do |val, idx|
           if idx.even?
             piece = row[idx]
-            row[idx] = "\u001b[47m #{piece} \u001b[0m"
+            row[idx] = grey_bg + " #{piece ? piece : ' '} " + reset_code
           else
             piece = row[idx]
-            row[idx] = "\u001b[42m #{piece} \u001b[0m"
+            row[idx] = green_bg + " #{piece ? piece : ' '} " + reset_code
           end
         end
       else
         row.each_with_index do |val, idx|
           if idx.odd?
             piece = row[idx]
-            row[idx] = "\u001b[47m #{piece} \u001b[0m"
+            row[idx] = grey_bg + " #{piece ? piece : ' '} " + reset_code
           else
             piece = row[idx]
-            row[idx] = "\u001b[42m #{piece} \u001b[0m"
+            row[idx] = green_bg + " #{piece ? piece : ' '} " + reset_code
           end
         end
       end
     end
   end
 
-  def add_pieces_to_display(pieces)
+  def grey_bg
+    "\u001b[47m"
+  end
+
+  def green_bg
+    "\u001b[42m"
+  end
+
+  def reset_code
+    "\u001b[0m"
+  end
+
+  def add_pieces_to_grid(pieces)
     pieces.each do |piece|
       row = piece.position[0]
       column = piece.position[1]
       grid[row][column] = piece.symbol
     end
+  end
+
+  def square(row, column)
+    grid[row][column]
+  end
+
+  def update_board
+    
   end
 end
