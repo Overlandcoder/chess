2226e5e0fc39f925364cf122b20410c6b934aead
Overlander <anuragnandal@hotmail.com>
2022-06-07 07:31:05 -0400

    Add test for #create_piece

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,12 @@
 require_relative '../lib/game'
 require_relative '../lib/piece'
 require_relative '../lib/player'
+require_relative '../lib/board'
 
 describe Game do
   subject(:game) { described_class.new }
   let(:player1) { double(Player, name: 'a', color: 'white') }
+  let(:board) { double(Board) }
 
   describe '#create_player' do
     before do
@@ -21,10 +23,18 @@ describe Game do
   end
 
   describe '#create_piece' do
+    let(:rook) { double(Rook, color: 'white', number: 0, board: board) }
+
     it 'sends .for to Piece' do
       allow(player1).to receive(:add_piece)
-      expect(Piece).to receive(:for).with('rook', 'white', 0)
-      game.create_piece('rook', 'white', player1, 0)
+      expect(Piece).to receive(:for).with('rook', 'white', 0, board)
+      game.create_piece('rook', 'white', player1, board, 0)
+    end
+
+    it 'sends #add_piece to Player' do
+      allow(Piece).to receive(:for).and_return(rook)
+      expect(player1).to receive(:add_piece).with(rook)
+      game.create_piece('rook', 'white', player1, board, 0)
     end
   end
 end
