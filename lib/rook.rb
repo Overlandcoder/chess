20c722d18a46c84b8c33dad73bd8931b2aec65ad
Overlander <anuragnandal@hotmail.com>
2022-06-07 09:18:00 -0400

    Add #valid_path? and #destination_reached

--- lib/rook.rb
+++ lib/rook.rb
@@ -1,9 +1,13 @@
-class Rook
-  attr_reader :color, :number, :position, :destination
+require 'pry-byebug'
+require_relative 'board'
 
-  def initialize(color, number)
+class Rook
+  attr_reader :color, :number, :position, :destination, :board, :row, :column
+
+  def initialize(color, number, board)
     @color = color
     @number = number
+    @board = board
     @position = [[7, 0], [7, 7]][number] if color == 'white'
     @position = [[0, 0 ], [0, 7]][number] if color == 'black'
   end
@@ -25,7 +29,7 @@ class Rook
   end
 
   def valid_move?
-    within_board? && path_empty? && (vertical_move? || horizontal_move?)
+    board.within_board? && path_empty? && (vertical_move? || horizontal_move?)
   end
 
   def path_empty?
@@ -34,16 +38,39 @@ class Rook
       end_row = destination[0]
       column = position[1]
 
-      (start_row..end_row).all? do |row|
-        grid[row][column] == "\u001b[47m   \u001b[0m" || grid[row][column] == "\u001b[42m   \u001b[0m"
+      (start_row - 1).downto(end_row).all? do |row|
+        board.square_at(row, column).nil? ||
+        board.square_at(end_row, column).color != board.square_at(start_row, column).color
       end
-    elsif horizontal_move?
-
+    # elsif horizontal_move?
+     # start_column = position[1]
+      # end_column = destination[1]
     end
   end
 
-  def within_board?
-    destination[0].between?(0, 7) && destination[1].between?(0, 7)
+  def valid_path?(direction)
+    @row = position[0]
+    @column = position[1]
+    square = board.square_at(row, column)
+
+    until destination_reached(direction)
+      direction == 'up' ? @row -= 1 : @row += 1
+      direction == 'right' ? @column += 1 : @column -= 1
+
+      return false if square
+
+      if destination_reached(direction)
+        return true if square.nil? || board.contains_opponent_piece?
+      end
+    end
+  end
+
+  def destination_reached(direction)
+    if direction == 'up' || direction == 'down'
+      row == destination[0]
+    elsif direction == 'right' || direction == 'left'
+      column == destination[1]
+    end
   end
 
   def vertical_move?
--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -12,6 +12,7 @@ describe Rook do
       before do
         rook.instance_variable_set(:@position, [7, 0])
         rook.instance_variable_set(:@destination, [4, 0])
+        allow(board).to receive(:within_board?).and_return(true)
         allow(board).to receive(:square_at)
       end
 
@@ -20,15 +21,87 @@ describe Rook do
       end
     end
 
-    context 'when given an invalid move as input' do
+    context 'when path is not empty' do
       before do
         rook.instance_variable_set(:@position, [7, 0])
         rook.instance_variable_set(:@destination, [0, 0])
-        allow(board).to receive(:square_at).and_return(nil, nil, nil, nil, nil, nil, "\u001b[30m\u265C")
+        allow(board).to receive(:within_board?).and_return(true)
+        allow(board).to receive(:square_at).and_return(nil, nil, nil, nil, nil, nil, rook)
       end
 
       it 'returns false' do
         expect(rook.valid_move?).to be false
+      end
+    end
+
+    context 'when given a diagonal (invalid) move as input' do
+      before do
+        rook.instance_variable_set(:@position, [7, 0])
+        rook.instance_variable_set(:@destination, [0, 7])
+        allow(board).to receive(:within_board?).and_return(true)
+        allow(rook).to receive(:path_empty?).and_return(true)
+      end
+
+      it 'returns false' do
+        expect(rook.valid_move?).to be false
+      end
+    end
+  end
+
+  describe '#vertical' do
+    context 'when direction is up and path is empty' do
+      before do
+        rook.instance_variable_set(:@position, [7, 0])
+        rook.instance_variable_set(:@destination, [4, 0])
+        allow(board).to receive(:square_at).and_return(nil, nil, nil)
+        allow(game).to receive(:contains_opponent_piece?).and_return(false)
+      end
+
+      it 'returns true' do
+        expect(rook.vertical('up')).to be true
+      end
+    end
+
+    context 'when direction is up and destination contains opponent piece' do
+      let(:opponent_rook) { double(Rook, color: 'black', number: 0, board: board) }
+      
+      before do
+        rook.instance_variable_set(:@position, [3, 0])
+        rook.instance_variable_set(:@destination, [0, 0])
+        allow(board).to receive(:square_at).and_return(nil, nil, nil, opponent_rook)
+        allow(game).to receive(:contains_opponent_piece?).and_return(true)
+      end
+
+      it 'returns true' do
+        expect(rook.vertical('up')).to be true
+      end
+    end
+
+    context 'when direction is down and path is empty' do
+      before do
+        rook.instance_variable_set(:@position, [0, 0])
+        rook.instance_variable_set(:@destination, [4, 0])
+        allow(board).to receive(:square_at).and_return(nil, nil, nil, nil)
+        allow(game).to receive(:contains_opponent_piece?).and_return(true)
+      end
+
+      it 'returns true' do
+        expect(rook.vertical('down')).to be true
+      end
+    end
+
+    context 'when direction is down and destination contains opponent piece' do
+      let(:opponent_rook) { double(Rook, color: 'black', number: 0, board: board) }
+      
+      before do
+        rook.instance_variable_set(:@position, [5, 0])
+        rook.instance_variable_set(:@destination, [7, 0])
+        allow(board).to receive(:square_at).and_return(nil, opponent_rook)
+        allow(game).to receive(:contains_opponent_piece?).and_return(true)
+      end
+
+      it 'returns true' do
+        expect(rook.vertical('down')).to be true
       end
     end
   end
