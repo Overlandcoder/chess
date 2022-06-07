34d1830f00f47ec017df3a512a569720999cd75f
Overlander <anuragnandal@hotmail.com>
2022-06-07 07:32:03 -0400

    Make grid private

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,11 @@
 require 'colorize'
+require 'pry-byebug'
 
 class Board
   attr_reader :grid
 
   def initialize
-    @grid = Array.new(8) { Array.new(8) }
+    create_grid
   end
 
   def display
@@ -16,7 +17,6 @@ class Board
     end
 
     column_headings
-    p grid[0].join
   end
 
   def column_headings
@@ -29,14 +29,14 @@ class Board
       if idx.even?
         row.each_index do |idx|
           piece = row[idx]
-          row[idx] = grey_bg + " #{piece ||= ' '} " + reset_code if idx.even?
-          row[idx] = green_bg + " #{piece ||= ' '} " + reset_code if idx.odd?
+          row[idx] = grey_bg + " #{piece ? piece.symbol : ' '} " + reset_code if idx.even?
+          row[idx] = green_bg + " #{piece ? piece.symbol : ' '} " + reset_code if idx.odd?
         end
       else
         row.each_index do |idx|
           piece = row[idx]
-          row[idx] = grey_bg + " #{piece ||= ' '} " + reset_code if idx.odd?
-          row[idx] = green_bg + " #{piece ||= ' '} " + reset_code if idx.even?
+          row[idx] = grey_bg + " #{piece ? piece.symbol : ' '} " + reset_code if idx.odd?
+          row[idx] = green_bg + " #{piece ? piece.symbol : ' '} " + reset_code if idx.even?
         end
       end
     end
@@ -54,19 +54,29 @@ class Board
     "\u001b[0m"
   end
 
-  def add_pieces_to_grid(pieces)
+  def attach_piece(pieces)
     pieces.each do |piece|
       row = piece.position[0]
       column = piece.position[1]
-      grid[row][column] = piece.symbol
+      grid[row][column] = piece
     end
   end
 
-  def square(row, column)
+  def square_at(row, column)
     grid[row][column]
+  end
+
+  def rook_path_empty?
+
   end
 
   def update_board
     
+  end
+
+  private
+
+  def create_grid
+    @grid = Array.new(8) { Array.new(8) }
   end
 end
