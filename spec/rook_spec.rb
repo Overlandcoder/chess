8b6fcd293d89aa7a79b1040a0ff9c8a74b667e54
Overlander <anuragnandal@hotmail.com>
2022-06-07 11:16:18 -0400

    Replace #valid_move? tests

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -7,48 +7,7 @@ describe Rook do
   let(:game) { double(Game) }
   let(:board) { double(Board) }
 
-  describe '#valid_move?' do
-    context 'when given a valid move as input' do
-      before do
-        rook.instance_variable_set(:@position, [7, 0])
-        rook.instance_variable_set(:@destination, [4, 0])
-        allow(board).to receive(:within_board?).and_return(true)
-        allow(board).to receive(:square_at)
-      end
-
-      it 'returns true' do
-        expect(rook.valid_move?).to be true
-      end
-    end
-
-    context 'when path is not empty' do
-      before do
-        rook.instance_variable_set(:@position, [7, 0])
-        rook.instance_variable_set(:@destination, [0, 0])
-        allow(board).to receive(:within_board?).and_return(true)
-        allow(board).to receive(:square_at).and_return(nil, nil, nil, nil, nil, nil, rook)
-      end
-
-      it 'returns false' do
-        expect(rook.valid_move?).to be false
-      end
-    end
-
-    context 'when given a diagonal (invalid) move as input' do
-      before do
-        rook.instance_variable_set(:@position, [7, 0])
-        rook.instance_variable_set(:@destination, [0, 7])
-        allow(board).to receive(:within_board?).and_return(true)
-        allow(rook).to receive(:path_empty?).and_return(true)
-      end
-
-      it 'returns false' do
-        expect(rook.valid_move?).to be false
-      end
-    end
-  end
-
-  describe '#vertical' do
+  describe '#valid_path?' do
     context 'when direction is up and path is empty' do
       before do
         rook.instance_variable_set(:@position, [7, 0])
@@ -58,7 +17,7 @@ describe Rook do
       end
 
       it 'returns true' do
-        expect(rook.vertical('up')).to be true
+        expect(rook.valid_path?('up')).to be true
       end
     end
 
@@ -73,7 +32,7 @@ describe Rook do
       end
 
       it 'returns true' do
-        expect(rook.vertical('up')).to be true
+        expect(rook.valid_path?('up')).to be true
       end
     end
 
@@ -86,7 +45,7 @@ describe Rook do
       end
 
       it 'returns true' do
-        expect(rook.vertical('down')).to be true
+        expect(rook.valid_path?('down')).to be true
       end
     end
 
@@ -101,7 +60,45 @@ describe Rook do
       end
 
       it 'returns true' do
-        expect(rook.vertical('down')).to be true
+        expect(rook.valid_path?('down')).to be true
+      end
+    end
+
+    context 'when direction is right and path is empty' do
+      before do
+        rook.instance_variable_set(:@position, [7, 0])
+        rook.instance_variable_set(:@destination, [7, 2])
+        allow(board).to receive(:square_at).and_return(nil, nil)
+        allow(game).to receive(:contains_opponent_piece?).and_return(false)
+      end
+
+      it 'returns true' do
+        expect(rook.valid_path?('right')).to be true
+      end
+    end
+
+    context 'when direction is left and destination contains opponent piece' do
+      let(:opponent_rook) { double(Rook, color: 'black', number: 0, board: board) }
+      
+      before do
+        rook.instance_variable_set(:@position, [7, 7])
+        rook.instance_variable_set(:@destination, [7, 5])
+        allow(board).to receive(:square_at).and_return(nil, nil)
+        allow(game).to receive(:contains_opponent_piece?).and_return(false)
+      end
+
+      it 'returns true' do
+        expect(rook.valid_path?('left')).to be true
+      end
+    end
+  end
+
+  describe '#valid_move?' do
+    context 'when the move is valid' do
+      it 'sends #within_board to Board' do
+        allow(rook).to receive(:valid_path?).and_return(true)
+        expect(board).to receive(:within_board?)
+        rook.valid_move?
       end
     end
   end
