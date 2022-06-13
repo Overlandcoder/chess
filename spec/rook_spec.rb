b0370d9b40f08c92d1edd82f3324cc20214b1a68
Overlander <anuragnandal@hotmail.com>
2022-06-13 09:12:34 -0400

    Refactor tests and remove a test

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -1,118 +1,133 @@
 require_relative '../lib/rook'
 require_relative '../lib/board'
 require_relative '../lib/game'
+require_relative '../lib/coordinate'
 
 describe Rook do
   subject(:rook) { described_class.new('white', 0, board) }
+
   let(:game) { instance_double(Game) }
   let(:board) { instance_double(Board) }
 
   describe '#valid_path?' do
     context 'when direction is up and path is empty' do
+      let(:destination) { instance_double(Coordinate, row: 4, col: 0) }
+
       before do
         rook.instance_variable_set(:@position, [7, 0])
-        rook.instance_variable_set(:@destination, [4, 0])
+        rook.instance_variable_set(:@destination, destination)
         allow(board).to receive(:square_at).and_return(nil, nil, nil)
-        allow(board).to receive(:contains_opponent_piece?).and_return(false)
+        allow(board).to receive(:opponent_piece?).and_return(false)
       end
 
       it 'returns true' do
-        expect(rook.valid_path?('up')).to be true
+        expect(rook.valid_path?).to be true
       end
     end
 
     context 'when direction is up and destination contains opponent piece' do
       let(:opponent_rook) { instance_double(described_class, color: 'black', number: 0, board: board) }
+      let(:destination) { instance_double(Coordinate, row: 0, col: 0) }
 
       before do
         rook.instance_variable_set(:@position, [3, 0])
-        rook.instance_variable_set(:@destination, [0, 0])
-        allow(board).to receive(:square_at).and_return(nil, nil, nil, opponent_rook)
-        allow(board).to receive(:contains_opponent_piece?).and_return(true)
+        rook.instance_variable_set(:@destination, destination)
+        allow(board).to receive(:square_at).and_return(nil, opponent_rook)
+        allow(board).to receive(:opponent_piece?).and_return(true)
       end
 
       it 'returns true' do
-        expect(rook.valid_path?('up')).to be true
+        expect(rook.valid_path?).to be true
       end
     end
 
     context 'when direction is down and path is empty' do
+      let(:destination) { instance_double(Coordinate, row: 4, col: 0) }
+
       before do
         rook.instance_variable_set(:@position, [0, 0])
-        rook.instance_variable_set(:@destination, [4, 0])
+        rook.instance_variable_set(:@destination, destination)
         allow(board).to receive(:square_at).and_return(nil, nil, nil, nil)
-        allow(board).to receive(:contains_opponent_piece?).and_return(true)
+        allow(board).to receive(:opponent_piece?).and_return(true)
       end
 
       it 'returns true' do
-        expect(rook.valid_path?('down')).to be true
+        expect(rook.valid_path?).to be true
       end
     end
 
     context 'when direction is down and destination contains opponent piece' do
-      let(:opponent_rook) { instance_double(described_class, color: 'black', number: 0, board: board) }
+      subject(:rook) { described_class.new('black', 0, board) }
+
+      let(:opponent_rook) { instance_double(described_class, color: 'white', number: 0, board: board) }
+      let(:destination) { instance_double(Coordinate, row: 7, col: 0) }
 
       before do
         rook.instance_variable_set(:@position, [5, 0])
-        rook.instance_variable_set(:@destination, [7, 0])
+        rook.instance_variable_set(:@destination, destination)
         allow(board).to receive(:square_at).and_return(nil, opponent_rook)
-        allow(board).to receive(:contains_opponent_piece?).and_return(true)
+        allow(board).to receive(:opponent_piece?).and_return(true)
       end
 
       it 'returns true' do
-        expect(rook.valid_path?('down')).to be true
+        expect(rook.valid_path?).to be true
       end
     end
 
     context 'when direction is right and path is empty' do
+      let(:destination) { instance_double(Coordinate, row: 7, col: 2) }
+
       before do
         rook.instance_variable_set(:@position, [7, 0])
-        rook.instance_variable_set(:@destination, [7, 2])
+        rook.instance_variable_set(:@destination, destination)
         allow(board).to receive(:square_at).and_return(nil, nil)
-        allow(board).to receive(:contains_opponent_piece?).and_return(false)
+        allow(board).to receive(:opponent_piece?).and_return(false)
       end
 
       it 'returns true' do
-        expect(rook.valid_path?('right')).to be true
+        expect(rook.valid_path?).to be true
       end
     end
 
     context 'when direction is left and destination contains opponent piece' do
       let(:opponent_rook) { instance_double(described_class, color: 'black', number: 0, board: board) }
+      let(:destination) { instance_double(Coordinate, row: 7, col: 5) }
 
       before do
         rook.instance_variable_set(:@position, [7, 7])
-        rook.instance_variable_set(:@destination, [7, 5])
+        rook.instance_variable_set(:@destination, destination)
         allow(board).to receive(:square_at).and_return(nil, nil)
-        allow(board).to receive(:contains_opponent_piece?).and_return(false)
+        allow(board).to receive(:opponent_piece?).and_return(false)
       end
 
       it 'returns true' do
-        expect(rook.valid_path?('left')).to be true
+        expect(rook.valid_path?).to be true
       end
     end
 
     context 'when direction is right and path is not empty' do
       let(:opponent_rook) { instance_double(Rook, color: 'black', number: 0, board: board) }
+      let(:destination) { instance_double(Coordinate, row: 7, col: 2) }
 
       before do
         rook.instance_variable_set(:@position, [7, 0])
-        rook.instance_variable_set(:@destination, [7, 2])
+        rook.instance_variable_set(:@destination, destination)
         allow(board).to receive(:square_at).and_return(opponent_rook, opponent_rook)
-        allow(board).to receive(:contains_opponent_piece?).and_return(true)
+        allow(board).to receive(:opponent_piece?).and_return(true)
       end
 
       it 'returns false' do
-        expect(rook.valid_path?('right')).to be false
+        expect(rook.valid_path?).to be false
       end
     end
   end
 
-  describe '#valid_move?' do
+  describe '#change_position' do
     context 'when the move is valid' do
-      it 'sends #within_board to Board' do
-        expect(board).to receive(:within_board?)
-        rook.valid_move?
+      it 'sends #update_board to Board' do
+        allow(rook).to receive(:valid_move?).and_return(true)
+        expect(board).to receive(:update_board).with(4, 0, rook)
+        rook.change_position([4, 0])
       end
     end
   end
