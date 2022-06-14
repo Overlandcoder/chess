60ed78806999021fc771d1dad8b9da467d5b85aa
Overlander <anuragnandal@hotmail.com>
2022-06-14 11:01:37 -0400

    Modify tests for #choose_piece and #own_piece?

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -39,36 +39,31 @@ describe Game do
 
   describe '#choose_piece' do
     context 'when user chooses their own piece' do
-      let(:white_rook) { instance_double(Rook, color: 'white', number: 0, board: board) }
-
       before do
-        allow(board).to receive(:square_at).and_return(white_rook)
+        allow(game).to receive(:own_piece?).and_return(true)
         allow(game).to receive(:current_player).and_return(player1)
         allow(game).to receive(:gets).and_return('A1')
         allow(game).to receive(:board).and_return(board)
       end
 
       it 'does not display error message' do
-        error_message = "That is your opponent's piece, please select your own piece!"
+        error_message = 'Please select your own piece!'
         expect(game).not_to receive(:puts).with(error_message)
         game.choose_piece
       end
     end
 
     context 'when the user chooses opponent piece, then their own piece' do
-      let(:black_rook) { instance_double(Rook, color: 'black', number: 1, board: board) }
-      let(:white_rook) { instance_double(Rook, color: 'white', number: 0, board: board) }
-
       before do
-        allow(board).to receive(:square_at).and_return(black_rook, white_rook)
+        allow(game).to receive(:own_piece?).and_return(false, true)
         allow(game).to receive(:current_player).and_return(player1)
         allow(game).to receive(:gets).and_return('H8', 'A1')
         allow(game).to receive(:board).and_return(board)
-        allow(game).to receive(:puts)
+        allow(game).to receive(:puts).with('Select a piece to move:')
       end
 
       it 'displays error message once' do
-        error_message = "That is your opponent's piece, please select your own piece!"
+        error_message = "Please select your own piece!"
         expect(game).to receive(:puts).with(error_message).once
         game.choose_piece
       end
@@ -76,22 +71,13 @@ describe Game do
   end
 
   describe '#own_piece?' do
-    let(:rook) { instance_double(Rook, color: 'white', number: 0, board: board) }
-
     before do
-      allow(game).to receive(:current_player).and_return(player1)
       allow(game).to receive(:board).and_return(board)
-      allow(board).to receive(:update_board).with(7, 0, rook)
+      allow(game).to receive(:current_player).and_return(player1)
     end
 
-    it 'sends #square_at to Board' do
-      expect(board).to receive(:square_at).with(7, 0).and_return(rook)
-      game.own_piece?(7, 0)
-    end
-
-    it 'sends #color to Rook' do
-      allow(board).to receive(:square_at).with(7, 0).and_return(rook)
-      expect(rook).to receive(:color).and_return('white')
+    it 'sends #opponent_piece? to Board' do
+      expect(board).to receive(:opponent_piece?).with(7, 0, 'white')
       game.own_piece?(7, 0)
     end
   end
