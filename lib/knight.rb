c2cd540820febeffe12c1cf9fb8756955d128d7c
Overlander <anuragnandal@hotmail.com>
2022-06-16 19:54:19 -0400

    Add #valid_move?, #set_destination and #update_position

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,7 +1,10 @@
 require_relative 'board'
 
 class Knight
-  attr_reader :color, :number, :position
+  attr_reader :color, :number, :position, :destination
+
+  POSSIBLE_MOVES = [[1, 2], [1, -2], [-1, 2], [-1, -2],
+                    [2, 1], [2, -1], [-2, 1], [-2, -1]]
 
   def initialize(color, number, board)
     @color = color
@@ -19,6 +22,22 @@ class Knight
       start_row, start_col = [[0, 1], [0, 6]][number]
       @position = Coordinate.new(row: start_row, col: start_col)
     end
+  end
+
+  def valid_move?
+    POSSIBLE_MOVES.any? do |move|
+      position.row + move[0] == destination.row &&
+      position.col + move[1] == destination.col
+    end
+  end
+
+  def set_destination(coordinate)
+    @destination = coordinate
+  end
+
+  def update_position
+    position.update_row(destination.row)
+    position.update_col(destination.col)
   end
 
   def symbol
