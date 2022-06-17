047a366007d4f37368e47d9e1c2b66d6126b26ac
Overlander <anuragnandal@hotmail.com>
2022-06-16 20:22:48 -0400

    Add tests for #update_position

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -18,4 +18,26 @@ describe Knight do
       knight.create_coordinate
     end
   end
+
+  describe '#update_position' do
+    let(:position) { instance_double(Coordinate, row: 7, col: 1) }
+    let(:destination) { instance_double(Coordinate, row: 5, col: 2) }
+
+    before do
+      allow(knight).to receive(:position).and_return(position)
+      allow(knight).to receive(:destination).and_return(destination)
+    end
+
+    it 'sends #update_row to Coordinate' do
+      allow(position).to receive(:update_col)
+      expect(position).to receive(:update_row).with(5)
+      knight.update_position
+    end
+
+    it 'sends #update_col to Coordinate' do
+      allow(position).to receive(:update_row)
+      expect(position).to receive(:update_col).with(2)
+      knight.update_position
+    end
+  end
 end
