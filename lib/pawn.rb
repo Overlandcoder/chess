ea3e0499a100a10f731acdac0247789b77faf410
Overlander <anuragnandal@hotmail.com>
2022-06-22 20:16:26 -0400

    Begin adding basic implementation

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -1,17 +1,49 @@
 class Pawn
-  attr_reader :color, :number, :position
+  attr_reader :color, :number, :position, :destination, :board
 
-  def initialize(color, number)
+  POSSIBLE_MOVES = [[-1, 0], [1, 0], [-2, 0], [2, 0]]
+
+  def initialize(color, number, board)
     @color = color
     @number = number
-    @position = [6, number] if color == 'white'
-    @position = [1, number] if color == 'black'
+    @board = board
+    create_coordinate
+    @possible_moves = []
+    @title = 'Pawn'
+    @moves_made = 0
+  end
+
+  def create_coordinate
+    case color
+    when :white
+      start_row = 6
+      start_col = [0, 1, 2, 3, 4, 5, 6, 7][number]
+      @position = Coordinate.new(row: start_row, col: start_col)
+    when :black
+      start_row = 1
+      start_col = [0, 1, 2, 3, 4, 5, 6, 7][number]
+      @position = Coordinate.new(row: start_row, col: start_col)
+    end
+  end
+
+  def valid_move?
+    @possible_moves.include?([destination.row, destination.col])
+  end
+
+  def set_destination(coordinate)
+    @destination = coordinate
+  end
+
+  def update_position(row = destination.row, col = destination.col)
+    position.update_row(row)
+    position.update_col(col)
+    @moves_made += 1
   end
 
   def symbol
-    if color == 'white'
-      "\u001b[37;1m\u265F"
-    elsif color == 'black'
+    if color == :white
+      "♟"
+    elsif color == :black
       "\u001b[30m\u265F"
     end
   end
