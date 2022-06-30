c5a66b184d30fb6787a40baee814e55e502dfba8
Overlander <anuragnandal@hotmail.com>
2022-06-30 15:33:50 -0400

    Rename some methods and begin implementing en passant

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -1,6 +1,8 @@
 class Pawn
   attr_reader :color, :number, :position, :destination, :board, :title,
-              :possible_moves, :moves_made
+              :possible_moves, :moves_made, :moved_two_squares
+
+  attr_accessor :capturing_en_passant
 
   def initialize(color, number, board)
     @color = color
@@ -18,7 +20,7 @@ class Pawn
     case color
     when :white
       start_col = [0, 1, 2, 3, 4, 5, 6, 7][number]
-      @position = Coordinate.new(row: 2, col: start_col)
+      @position = Coordinate.new(row: 6, col: start_col)
     when :black
       start_col = [0, 1, 2, 3, 4, 5, 6, 7][number]
       @position = Coordinate.new(row: 1, col: start_col)
@@ -34,6 +36,7 @@ class Pawn
   end
 
   def update_position(row = destination.row, col = destination.col)
+    two_square_move?
     position.update_row(row)
     position.update_col(col)
     @moves_made += 1
@@ -41,8 +44,8 @@ class Pawn
 
   def generate_possible_moves
     @possible_moves.clear
-    remove_double_moves_if_moves_made
-    remove_double_moves_if_path_blocked
+    remove_two_square_moves_if_moves_made
+    remove_two_square_moves_if_path_blocked
 
     @moves.each do |move|
       row = position.row + move[0]
@@ -51,14 +54,14 @@ class Pawn
 
       @possible_moves << [row, col] if board.square_at(row, col).nil?
     end
-    attacking_moves.each { |move| @possible_moves << move }
+    add_capturing_moves
   end
 
-  def remove_double_moves_if_moves_made
+  def remove_two_square_moves_if_moves_made
     @moves.delete_if { |move| (move[0].abs > 1) && moves_made.positive? }
   end
   
-  def remove_double_moves_if_path_blocked
+  def remove_two_square_moves_if_path_blocked
     @moves.delete_if do |move|
       first_square_row = position.row - 1 if color == :white
       first_square_row = position.row + 1 if color == :black
@@ -67,12 +70,12 @@ class Pawn
     end
   end
 
-  def attacking_moves(removing_king_checks = false)
-    pawn_attack_moves = [[-1, 1], [-1, -1]] if color == :white
-    pawn_attack_moves = [[1, 1], [1, -1]] if color == :black
+  def add_capturing_moves(removing_king_checks = false)
+    pawn_capture_moves = [[-1, 1], [-1, -1]] if color == :white
+    pawn_capture_moves = [[1, 1], [1, -1]] if color == :black
     moves = []
 
-    pawn_attack_moves.each do |move|
+    pawn_capture_moves.each do |move|
       row = position.row + move[0]
       col = position.col + move[1]
       moves << [row, col] if row.between?(0, 7) &&
@@ -82,9 +85,22 @@ class Pawn
     moves
   end
 
-  def attacking_moves_only
+  def capturing_moves_only
     @possible_moves.clear
-    attacking_moves(true)
+    add_capturing_moves(true)
+  end
+
+  def add_en_passant_moves
+    left_piece = board.square_at(position.row, position.col - 1)
+    right_piece = board.square_at(position.row, position.col + 1)
+    next_row = position.row + 1 if color == :black
+    next_row = position.row - 1 if color == :white
+
+    if left_piece && left_piece.moved_two_squares
+      @possible_moves << [next_row, position.col - 1]
+    elsif right_piece && right_piece.moved_two_squares
+      @possible_moves << [next_row, position.col + 1]
+    end
   end
 
   def opponent?(removing_king_checks = false, row, col)
@@ -96,6 +112,14 @@ class Pawn
   def can_be_promoted?
     (position.row == 0 && color == :white) ||
     (position.row == 7 && color == :black)
+  end
+
+  def two_square_move?
+    if (position.row - destination.row).abs == 2
+      @moved_two_squares = true
+    else
+      @moved_two_squares = false
+    end
   end
 
   def symbol
