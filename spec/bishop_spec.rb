3e62c82b9753a5238d5d684ac69eb0b90f0cb3f6
Overlander <anuragnandal@hotmail.com>
2022-07-18 20:24:20 -0400

    Add tests for #update_position

--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -18,4 +18,26 @@ describe Bishop do
       bishop.create_coordinate
     end
   end
+
+  describe '#update_position' do
+    let(:position) { instance_double(Coordinate, row: 7, col: 2) }
+    let(:destination) { instance_double(Coordinate, row: 5, col: 2) }
+
+    before do
+      allow(bishop).to receive(:position).and_return(position)
+      allow(bishop).to receive(:destination).and_return(destination)
+    end
+
+    it 'sends #update_row to Coordinate' do
+      allow(position).to receive(:update_col)
+      expect(position).to receive(:update_row).with(5)
+      bishop.update_position
+    end
+
+    it 'sends #update_col to Coordinate' do
+      allow(position).to receive(:update_row)
+      expect(position).to receive(:update_col).with(2)
+      bishop.update_position
+    end
+  end
 end
