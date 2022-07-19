893c9d01b2f58ca3ca33fe57b1e059b3eb1a8241
Overlander <anuragnandal@hotmail.com>
2022-07-19 13:40:25 -0400

    Delete tests for deleted methods, modify tests for #update_position

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -10,120 +10,32 @@ describe Rook do
   let(:board) { instance_double(Board) }
 
   describe '#update_position' do
-    context 'when direction is up' do
-      let(:position) { instance_double(Coordinate, row: 7, col: 0) }
-      let(:destination) { instance_double(Coordinate, row: 0, col: 0) }
-
-      before do
-        allow(rook).to receive(:position).and_return(position)
-        allow(rook).to receive(:destination).and_return(destination)
-        rook.instance_variable_set(:@direction, 'up')
-      end
-
-      it 'sends #update_row to Coordinate' do
-        expect(position).to receive(:update_row).with(0)
-        rook.update_position
-      end
-    end
-
-    context 'when direction is up' do
-      let(:position) { instance_double(Coordinate, row: 7, col: 0) }
-      let(:destination) { instance_double(Coordinate, row: 0, col: 0) }
-
-      before do
-        allow(rook).to receive(:position).and_return(position)
-        allow(rook).to receive(:destination).and_return(destination)
-        rook.instance_variable_set(:@direction, 'up')
-        allow(position).to receive(:update_row)
-      end
-
-      it 'sends #row to Coordinate' do
-        expect(destination).to receive(:row)
-        rook.update_position
-      end
-    end
-
-    context 'when direction is right' do
-      let(:position) { instance_double(Coordinate, row: 7, col: 0) }
-      let(:destination) { instance_double(Coordinate, row: 7, col: 4) }
-
-      before do
-        allow(rook).to receive(:position).and_return(position)
-        allow(rook).to receive(:destination).and_return(destination)
-        rook.instance_variable_set(:@direction, 'right')
-      end
-
-      it 'sends #update_col to Coordinate' do
-        expect(position).to receive(:update_col).with(4)
-        rook.update_position
-      end
-    end
-
-    context 'when direction is right' do
-      let(:position) { instance_double(Coordinate, row: 7, col: 0) }
-      let(:destination) { instance_double(Coordinate, row: 7, col: 5) }
-
-      before do
-        allow(rook).to receive(:position).and_return(position)
-        allow(rook).to receive(:destination).and_return(destination)
-        rook.instance_variable_set(:@direction, 'right')
-        allow(position).to receive(:update_col)
-      end
-
-      it 'sends #col to Coordinate' do
-        expect(destination).to receive(:col)
-        rook.update_position
-      end
-    end
-  end
-
-  describe '#valid_move?' do
     let(:position) { instance_double(Coordinate, row: 7, col: 0) }
     let(:destination) { instance_double(Coordinate, row: 2, col: 0) }
 
-    before do
-      allow(rook).to receive(:position).and_return(position)
-      allow(rook).to receive(:destination).and_return(destination)
-      allow(rook).to receive(:valid_path?).and_return(true)
-    end
-
-    it 'sends #row to Coordinate' do
-      expect(position).to receive(:row)
-      rook.valid_move?
-    end
-
-    it 'sends #col to Coordinate' do
-      expect(position).to receive(:col)
-      rook.valid_move?
-    end
-  end
-
-  describe '#destination_reached?' do
-    context 'when direction is up' do
-      let(:destination) { instance_double(Coordinate, row: 7, col: 0) }
-
+    context '' do
       before do
+        allow(rook).to receive(:position).and_return(position)
         allow(rook).to receive(:destination).and_return(destination)
-        allow(rook).to receive(:direction).and_return('up')
+        allow(position).to receive(:update_col)
       end
 
-      it 'sends #row to Coordinate' do
-        expect(destination).to receive(:row)
-        rook.destination_reached?
+      it 'sends #update_row to Coordinate' do
+        expect(position).to receive(:update_row).with(2)
+        rook.update_position
       end
     end
 
-    context 'when direction is left' do
-      let(:destination) { instance_double(Coordinate, row: 7, col: 7) }
-
+    context '' do
       before do
+        allow(rook).to receive(:position).and_return(position)
         allow(rook).to receive(:destination).and_return(destination)
-        allow(rook).to receive(:direction).and_return('left')
+        allow(position).to receive(:update_row)
       end
 
-      it 'sends #col to Coordinate' do
-        expect(destination).to receive(:col)
-        rook.destination_reached?
+      it 'sends #update_col to Coordinate' do
+        expect(position).to receive(:update_col).with(0)
+        rook.update_position
       end
     end
   end
