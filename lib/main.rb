551c5185d09a1af096a7e197e475fbaa913d7047
Overlander <anuragnandal@hotmail.com>
2022-07-22 12:26:13 -0400

    Add require_relative statements and statement that starts a new game

---
+++ lib/main.rb
@@ -0,0 +1,13 @@
+require_relative 'game'
+require_relative 'player'
+require_relative 'board'
+require_relative 'piece'
+require_relative 'coordinate'
+require_relative 'rook'
+require_relative 'knight'
+require_relative 'bishop'
+require_relative 'queen'
+require_relative 'king'
+require_relative 'pawn'
+
+Game.new.play_game
