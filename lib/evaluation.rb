73c7589ce238231be2eaf7c974ad6ee37cd6c3e7
Overlander <anuragnandal@hotmail.com>
2022-07-24 14:44:36 -0400

    Create deep copy of board in #opponent_moves_list, and rename things

--- lib/evaluation.rb
+++ lib/evaluation.rb
@@ -31,46 +31,46 @@ class Evaluation
   def king_in_check?
     row = king.position.row
     col = king.position.col
-    opponent_moves.any? { |opponent_move| opponent_move == [row, col] }
+    opponent_moves_list.any? { |opponent_move| opponent_move == [row, col] }
   end
 
   def remove_king_checks
     king.possible_moves.delete_if { |king_move|
-      opponent_moves.include?(king_move)
+      opponent_moves_list.include?(king_move)
     }
   end
 
-  def opponent_moves
-    possible_moves = []
+  def opponent_moves_list
+    @board_copy = Marshal.load(Marshal.dump(board))
+    opponent_moves = []
     (0..7).each do |row|
       (0..7).each do |col|
         opponent_piece = @board_copy.square_at(row, col)
         next if opponent_piece.nil? || opponent_piece.color == @color
 
         if opponent_piece.is_a?(Pawn)
-          possible_moves << opponent_piece.capturing_moves_only
+          opponent_moves << opponent_piece.capturing_moves_only
         else
           opponent_piece.generate_possible_moves(@board_copy, true)
-          possible_moves << opponent_piece.possible_moves
+          opponent_moves << opponent_piece.possible_moves
         end
       end
     end
-    possible_moves.flatten(1)
+    opponent_moves.flatten(1)
   end
 
   def checkmate?
     no_moves_left? && king_in_check?
   end
 
-  def no_moves_left?(checking_stalemate = false)
+  def no_moves_left?
     board.pieces(@color).all? do |piece|
-      # binding.pry if checking_stalemate
       piece.generate_possible_moves(@board)
       piece.possible_moves.length.zero?
     end
   end
 
   def stalemate?
-    no_moves_left?(true) && !king_in_check?
+    no_moves_left? && !king_in_check?
   end
 end
