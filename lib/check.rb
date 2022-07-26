dbcb6b77e9d9d45ef8ea9464a8dc266c5952cb6f
Overlander <anuragnandal@hotmail.com>
2022-07-26 19:53:39 -0400

    Add many small fixes and adjustments

--- lib/check.rb
+++ lib/check.rb
@@ -1,6 +1,9 @@
 module Check
+  attr_reader :board
+
   def remove_check_moves(current_piece, board)
     @current_piece = current_piece
+    @current_player_color = current_piece.color
     current_row = @current_piece.position.row
     current_col = @current_piece.position.col
     moves_to_delete = []
@@ -8,15 +11,15 @@ module Check
     @current_piece.possible_moves.each do |move|
       @board_copy = Marshal.load(Marshal.dump(board))
       simulate_move(current_row, current_col, move)
-      moves_to_delete << move if king_in_check? && !@current_piece.is_a?(King)
-      remove_king_checks
+      moves_to_delete << move if king_in_check?(board) && !@current_piece.is_a?(King)
+      remove_king_checks(board)
     end
 #
     moves_to_delete.each { |move| @current_piece.possible_moves.delete(move) }
   end
 
-  def king
-    board.pieces(@current_piece.color).find { |piece| piece.is_a?(King) }
+  def king(board)
+    board.pieces(@current_player_color).find { |piece| piece.is_a?(King) }
   end
 
   def simulate_move(row, col, move)
@@ -24,14 +27,14 @@ module Check
     @board_copy.place(move[0], move[1], @current_piece)
   end
 
-  def king_in_check?
-    row = king.position.row
-    col = king.position.col
+  def king_in_check?(board)
+    row = king(board).position.row
+    col = king(board).position.col
     opponent_moves.any? { |opponent_move| opponent_move == [row, col] }
   end
 
-  def remove_king_checks
-    king.possible_moves.delete_if { |king_move|
+  def remove_king_checks(board)
+    king(board).possible_moves.delete_if { |king_move|
       opponent_moves.include?(king_move)
     }
   end
@@ -47,7 +50,7 @@ module Check
         if opponent_piece.is_a?(Pawn)
           possible_moves << opponent_piece.capturing_moves_only
         else
-          opponent_piece.generate_possible_moves(@board_copy)
+          opponent_piece.generate_possible_moves(@board_copy, true)
           possible_moves << opponent_piece.possible_moves
         end
       end
@@ -56,18 +59,19 @@ module Check
   end
 
   def checkmate?
+    @current_player_color = @current_player.color
     no_moves_left? && king_in_check?
   end
 
-  def no_moves_left?
-    board.pieces(current_player.color).all? do |piece|
+  def no_moves_left?(checking_stalemate = false)
+    board.pieces(@current_player_color).all? do |piece|
+      binding.pry if checking_stalemate
       piece.generate_possible_moves(board)
-      remove_check_moves(piece)
       piece.possible_moves.length.zero?
     end
   end
 
   def stalemate?
-    no_moves_left? && !king_in_check?
+    no_moves_left?(true) && !king_in_check?
   end
 end
