45b2d82569a3f5b67c41c4055f2df5b839724b74
Overlander <anuragnandal@hotmail.com>
2022-07-27 18:55:32 -0400

    Add more tests for #generate_possible_moves

--- spec/queen_spec.rb
+++ spec/queen_spec.rb
@@ -63,6 +63,7 @@ describe Queen do
       let(:queen) { board.square_at(4, 2) }
 
       before do
+        queen.update_position(4, 2)
         queen.generate_possible_moves(board)
       end
 
@@ -81,58 +82,55 @@ describe Queen do
       it 'can move to an empty square on the path' do
         expect(queen.possible_moves).to include([3, 2])
       end
-    end
 
-    context 'when a leftward horizontal path to an opponent piece is clear' do
-      fen_string = 'rnbqkbnr/pp1ppppp/8/8/1p1R4/8/8/1NBQKBNR'
-      let(:board) { Fen.new.to_board(fen_string) }
-      let(:queen) { board.square_at(4, 3) }
-
-      before do
-        queen.update_position(4, 3)
-        queen.generate_possible_moves(board)
-      end
-
-      it 'can capture opponent piece' do
-        expect(queen.possible_moves).to include([4, 1])
-      end
-
-      it 'can move to an empty square on the path' do
-        expect(queen.possible_moves).to include([4, 2])
+      it 'has 17 possible moves in this scenario' do
+        expect(queen.possible_moves.length).to eq(17)
       end
     end
 
-    context 'when moving a piece may lead to check' do
-      fen_string = 'rnb1kbnr/ppppqppp/8/8/1p2R3/8/8/1NBQKBNR'
+    context 'when moving the piece may lead to check' do
+      fen_string = 'r1n1kbnr/pp2pppp/2p1q3/8/8/4Q3/PPP2PPP/RNB1KBNR'
       let(:board) { Fen.new.to_board(fen_string) }
-      let(:queen) { board.square_at(4, 4) }
+      let(:queen) { board.square_at(5, 4) }
 
       before do
-        queen.update_position(4, 4)
+        queen.update_position(5, 4)
         queen.generate_possible_moves(board)
       end
 
       it 'cannot make a move that would lead to check' do
-        expect(queen.possible_moves).not_to include([4, 1])
+        expect(queen.possible_moves).not_to include([4, 3])
       end
 
       it 'can capture opponent piece' do
-        expect(queen.possible_moves).to include([1, 4])
+        expect(queen.possible_moves).to include([2, 4])
+      end
+
+      it 'has 4 possible moves' do
+        expect(queen.possible_moves.length).to eq(4)
       end
     end
 
-    context 'when king is in check' do
-      fen_string = 'rnb1kbnr/pppp1ppp/8/8/4R2q/5P2/PPPP2PP/RNBQKBN1'
+    context 'when king is in check and path can be blocked' do
+      fen_string = 'r1n1kbnr/pp2pppp/4q3/8/6Q1/8/PPP2PPP/RNB1KBNR'
       let(:board) { Fen.new.to_board(fen_string) }
-      let(:queen) { board.square_at(4, 4) }
+      let(:queen) { board.square_at(4, 6) }
 
       before do
-        queen.update_position(4, 4)
+        queen.update_position(4, 6)
         queen.generate_possible_moves(board)
       end
 
-      it 'can only move to capture the piece putting king in check' do
-        expect(queen.possible_moves.flatten).to eq([4, 7])
+      it 'is allowed to block the path to own king' do
+        expect(queen.possible_moves).to include([4, 4])
+      end
+
+      it 'can capture the piece putting own king in check' do
+        expect(queen.possible_moves).to include([2, 4])
+      end
+
+      it 'has no other possible moves except moves that remove check' do
+        expect(queen.possible_moves.length).to eq(3)
       end
     end
 
