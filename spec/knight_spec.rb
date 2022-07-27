6f61e32d6a036aab7b697ec00aae8d687829f152
Overlander <anuragnandal@hotmail.com>
2022-07-27 15:55:34 -0400

    Add some tests for Knight possible moves

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -1,19 +1,16 @@
+require_relative '../lib/piece'
 require_relative '../lib/knight'
-require_relative '../lib/game'
+require_relative '../lib/board'
 require_relative '../lib/coordinate'
+require_relative '../lib/fen'
+require_relative '../lib/evaluation'
 
 describe Knight do
-  subject(:knight) { described_class.new('white', 0, board) }
-
-  let(:game) { instance_double(Game) }
-  let(:board) { instance_double(Board) }
+  subject(:knight) { described_class.new(:white, 0) }
 
   describe '#create_coordinate' do
-    before do
-      allow(knight).to receive(:number).and_return(0)
-    end
-
     it 'sends #new to Coordinate' do
+      allow(knight).to receive(:number).and_return(0)
       expect(Coordinate).to receive(:new).with(row: 7, col: 1)
       knight.create_coordinate
     end
@@ -38,6 +35,53 @@ describe Knight do
       allow(position).to receive(:update_row)
       expect(position).to receive(:update_col).with(2)
       knight.update_position
+    end
+  end
+
+  describe '#generate_possible_moves' do
+    context 'when any move would place king in check' do
+      fen_string = 'r1bq1bnr/pppp1ppp/4k3/4n3/8/4Q3/PPPPPPPP/RNB1KBNR'
+      let(:board) { Fen.new.to_board(fen_string) }
+      let(:knight) { board.square_at(3, 4) }
+
+      before do
+        knight.update_position(3, 4)
+        knight.generate_possible_moves(board)
+      end
+
+      it 'has no possible moves' do
+        expect(knight.possible_moves.length).to eq(0)
+      end
+    end
+
+    context 'when king is in check and piece causing check must be captured' do
+      fen_string = 'r1bq1bnr/pppp1ppp/4k3/4n3/6Q1/8/PPPPPPPP/RNB1KBNR'
+      let(:board) { Fen.new.to_board(fen_string) }
+      let(:knight) { board.square_at(3, 4) }
+
+      before do
+        knight.update_position(3, 4)
+        knight.generate_possible_moves(board)
+      end
+
+      it 'can only move to capture the piece causing check' do
+        expect(knight.possible_moves.flatten).to eq([4, 6])
+      end
+    end
+
+    context 'when all possible move squares are empty' do
+      fen_string = 'r1bqkbnr/pp5p/8/4n3/8/8/2PPPPP1/RNB1KBNR'
+      let(:board) { Fen.new.to_board(fen_string) }
+      let(:knight) { board.square_at(3, 4) }
+
+      before do
+        knight.update_position(3, 4)
+        knight.generate_possible_moves(board)
+      end
+
+      it 'has a maximum of 8 possible moves' do
+        expect(knight.possible_moves.length).to eq(8)
+      end
     end
   end
 end
