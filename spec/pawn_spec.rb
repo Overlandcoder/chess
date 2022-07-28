65226ef8622011cf9732923b8222be61d0447935
Overlander <anuragnandal@hotmail.com>
2022-07-28 13:32:26 -0400

    Add tests for #generate_possible_moves

--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -41,4 +41,80 @@ describe Pawn do
       pawn.create_coordinate
     end
   end
+
+  describe '#generate_possible_moves' do
+    context 'when the next square contains own piece' do
+      fen_string = 'rnbqkbnr/pppppppp/8/8/8/1P6/PP1PPPPP/RNBQKBNR'
+      let(:board) { Fen.new.to_board(fen_string) }
+      let(:pawn) { board.square_at(6, 1) }
+
+      before do
+        pawn.generate_possible_moves(board)
+      end
+
+      it 'has no possible moves' do
+        expect(pawn.possible_moves.length).to eq(0)
+      end
+    end
+
+    context 'when piece has made 0 moves from starting position' do
+      fen_string = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'
+      let(:board) { Fen.new.to_board(fen_string) }
+      let(:pawn) { board.square_at(1, 1) }
+
+      before do
+        pawn.instance_variable_set(:@moves_made, 0)
+        pawn.generate_possible_moves(board)
+      end
+
+      it 'has 2 possible moves' do
+        expect(pawn.possible_moves.sort).to eq([[2, 1], [3, 1]].sort)
+      end
+    end
+
+    context 'when piece has made 1 move' do
+      fen_string = 'rnbqkbnr/p1pppppp/1p6/8/8/8/PPPPPPPP/RNBQKBNR'
+      let(:board) { Fen.new.to_board(fen_string) }
+      let(:pawn) { board.square_at(2, 1) }
+
+      before do
+        pawn.update_position(2, 1)
+        pawn.generate_possible_moves(board)
+      end
+
+      it 'has 1 possible move' do
+        expect(pawn.possible_moves).to eq([[3, 1]].sort)
+      end
+    end
+
+    context 'when a piece can be captured' do
+      fen_string = 'rnbqkbnr/p1pppppp/1p6/2P5/8/8/PP1PPPPP/RNBQKBNR'
+      let(:board) { Fen.new.to_board(fen_string) }
+      let(:pawn) { board.square_at(2, 1) }
+
+      before do
+        pawn.update_position(2, 1)
+        pawn.generate_possible_moves(board)
+      end
+
+      it 'can capture the piece or make a regular move' do
+        expect(pawn.possible_moves.sort).to eq([[3, 1], [3, 2]].sort)
+      end
+    end
+
+    context 'when path ahead blocked by opponent piece' do
+      fen_string = 'rnbqkbnr/p1pppppp/8/1p6/1P6/8/PP1PPPPP/RNBQKBNR'
+      let(:board) { Fen.new.to_board(fen_string) }
+      let(:pawn) { board.square_at(4, 1) }
+
+      before do
+        pawn.update_position(4, 1)
+        pawn.generate_possible_moves(board)
+      end
+
+      it 'has no possible moves' do
+        expect(pawn.possible_moves.length).to eq(0)
+      end
+    end
+  end
 end
