20f0cbf32831576f92984ae95550da80ea79e533
Overlander <anuragnandal@hotmail.com>
2022-07-29 15:35:19 -0400

    Use attr_accessor for can_castle instead of attr_reader

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,8 +1,8 @@
 class Piece
-  attr_reader :color, :number, :position, :board, :moved_two_squares, :can_castle
+  attr_reader :color, :number, :position, :board, :moved_two_squares
 
   attr_accessor :possible_moves, :destination, :capturing_en_passant,
-                :moved_last, :moves_made
+                :moved_last, :moves_made, :can_castle
 
   def self.for(piece_type, color, number)
     case piece_type
