fd680293dde05c568eec186af2d1382cfb3d588f
Overlander <anuragnandal@hotmail.com>
2022-07-29 16:11:13 -0400

    Add tests for checkmate

--- spec/evaluation_spec.rb
+++ spec/evaluation_spec.rb
@@ -16,4 +16,26 @@ describe Evaluation do
       expect(evaluation.stalemate?).to be true
     end
   end
+
+  describe '#checkmate?' do
+    context 'when white king is in checkmate' do
+      fen_string = 'rnb1kbnr/pppppppp/8/8/7q/4P3/PPPPP2P/RNBQKBNR w'
+      let(:board) { Fen.new.to_board(fen_string) }
+      subject(:evaluation) { described_class.new(board, :white) }
+
+      it 'results in checkmate' do
+        expect(evaluation.checkmate?).to be true
+      end
+    end
+
+    context 'when black king is in checkmate' do
+      fen_string = 'rn1qkbnr/ppppp2p/8/7Q/8/4P3/PPPPP2P/RNB1KBNR b'
+      let(:board) { Fen.new.to_board(fen_string) }
+      subject(:evaluation) { described_class.new(board, :black) }
+
+      it 'results in checkmate' do
+        expect(evaluation.checkmate?).to be true
+      end
+    end
+  end
 end
